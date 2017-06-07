82c0041172674fe0226985aeea627c7554d13188
Yeontae Kim <ytk007@gmail.com>
2017-06-07 15:28:11 -0700

    basic settings for the app

---
+++ OnTheMap/LoginVC.swift
@@ -0,0 +1,34 @@
+//
+//  LoginVC.swift
+//  OnTheMap
+//
+//  Created by Yeontae Kim on 6/6/17.
+//  Copyright © 2017 YTK. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackground()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func configureBackground() {
+        let backgroundGradient = CAGradientLayer()
+        let colorTop = UIColor(red: 0.345, green: 0.839, blue: 0.988, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 0.023, green: 0.569, blue: 0.910, alpha: 1.0).cgColor
+        backgroundGradient.colors = [colorTop, colorBottom]
+        backgroundGradient.locations = [0.0, 1.0]
+        backgroundGradient.frame = view.frame
+        view.layer.insertSublayer(backgroundGradient, at: 0)
+    }
+
+}
+
---
+++ OnTheMap/UdacityLocation.swift
@@ -0,0 +1,9 @@
+//
+//  UdacityLocation.swift
+//  OnTheMap
+//
+//  Created by Yeontae Kim on 6/6/17.
+//  Copyright © 2017 YTK. All rights reserved.
+//
+
+import Foundation
