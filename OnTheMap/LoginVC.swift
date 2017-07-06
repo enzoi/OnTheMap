63d69a144c9dedeb5fa1a4f4d7208edca19ebcc5
Yeontae Kim <ytk007@gmail.com>
2017-07-06 14:50:34 -0700

    fixed Facebook login

--- OnTheMap/LoginVC.swift
+++ OnTheMap/LoginVC.swift
@@ -64,6 +64,9 @@ class LoginVC: UIViewController, LoginButtonDelegate {
     
     func loginButtonDidCompleteLogin(_ loginButton: LoginButton, result: LoginResult) {
         print("successfully logged in")
+        
+        // Get Public User Data Using Access Token
+        self.postSessionWithFB()
         self.completeLogin()
     }
     
@@ -99,22 +102,6 @@ class LoginVC: UIViewController, LoginButtonDelegate {
         
     }
     
-    @IBAction func FBLoginButtonClicked() {
-        let loginManager = LoginManager()
-        
-        loginManager.logIn([ .publicProfile ], viewController: self) { loginResult in
-            switch loginResult {
-            case .failed(let error):
-                print(error)
-            case .cancelled:
-                print("User cancelled login.")
-            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
-                print("Logged in!")
-                self.completeLogin()
-            }
-        }
-    }
-    
     private func completeLogin() {
         performUIUpdatesOnMain {
             self.debugTextLabel.text = ""
@@ -126,6 +113,68 @@ class LoginVC: UIViewController, LoginButtonDelegate {
     }
     
     // MARK: Udacity Get a Session
+    
+    private func postSessionWithFB() {
+    
+        var accessToken: String = ""
+        if let token = AccessToken.current {
+            accessToken = token.authenticationToken 
+        }
+        
+        let request = NSMutableURLRequest(url: URL(string: "https://www.udacity.com/api/session")!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = "{\"facebook_mobile\": {\"access_token\": \"\(accessToken);\"}}".data(using: String.Encoding.utf8)
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: request as URLRequest) { data, response, error in
+            
+            // if an error occurs, print it and re-enable the UI
+            func displayError(_ error: String) {
+                print(error)
+                performUIUpdatesOnMain {
+                    self.setUIEnabled(true)
+                    self.debugTextLabel.text = "Login Failed."
+                }
+            }
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                displayError("There was an error with your request: \(error!)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                displayError("Your request returned a status code other than 2xx!")
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                displayError("No data was returned by the request!")
+                return
+            }
+            
+            let range = Range(5..<data.count)
+            let newData = data.subdata(in: range) /* subset response data! */
+            print(NSString(data: newData, encoding: String.Encoding.utf8.rawValue)!)
+        
+            let json = try? JSONSerialization.jsonObject(with: newData, options: []) as! [String: Any]
+            
+            if let dictionary = json {
+                if let account = dictionary["account"] as? [String: Any] {
+                    // access individual value in dictionary
+                    
+                    self.getPublicUserData(user_id: account["key"] as! String)
+                    
+                }
+            }
+        }
+        
+        task.resume()
+    }
     
     private func postSession() {
         
@@ -198,7 +247,7 @@ class LoginVC: UIViewController, LoginButtonDelegate {
             
             let range = Range(5..<data!.count)
             let newData = data?.subdata(in: range) /* subset response data! */
-            //print(NSString(data: newData!, encoding: String.Encoding.utf8.rawValue)!)
+            print(NSString(data: newData!, encoding: String.Encoding.utf8.rawValue)!)
             
             let json = try? JSONSerialization.jsonObject(with: newData!, options: []) as! [String: Any]
             
